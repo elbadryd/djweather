b68bdbbad1de247f5972ac0bbbd8f20be088fd84
Dario <elbadry.d@gmail.com>
2019-02-22 14:57:28 -0600

    error handling on input added

--- djweather/views.py
+++ djweather/views.py
@@ -6,20 +6,24 @@ from .forms import CityForm
 
 # Create your views here.
 def index(request):
-    print(request)
     key = os.environ['WEATHER_API']
     cities = City.objects.all()
     form = CityForm()
     weather_data = []
-    if request.method == 'POST': # only true if form is submitted
-        form = CityForm(request.POST) # add actual request data to form for processing
-        form.save() # will validate and save if validate
-    form = CityForm()
-    if request.method == "DELETE":
-        print(request, 'req')
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={key}'
+    if request.method == 'POST':
+        city_name = request.POST['name']
+        res = requests.get(url.format(city=city_name,key=key)).json()
+        print (res['cod'])
+        if res['cod'] != 200:
+            form = CityForm()
+        else:
+            form = CityForm(request.POST) # add actual request data to form for processing
+            form.save() # will validate and save if validate
+            form = CityForm()
     for city in cities:
-        url = f'http://api.openweathermap.org/data/2.5/weather?q={city.name}&units=imperial&appid={key}'
-        city_weather = requests.get(url).json()
+        
+        city_weather = requests.get(url.format(city=city.name,key=key)).json()
         weather = {
             'city' : city,
             'temperature' : city_weather['main']['temp'],
